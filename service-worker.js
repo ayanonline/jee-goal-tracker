79622b9ede70fa81fd3f4c55edbd806eb56dbdcd
Ayan Sarkar <kingstar324576@gmail.com>
2026-07-06 20:55:49 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,11 +3,16 @@ const CACHE_NAME = "pretrack-v1";
 const urlsToCache = [
     "/",
     "/index.html",
-    "/style.css",
-    "/script.js"
+    "/css/style.css",
+    "/js/script.js",
+    "/favicon/android-icon-192x192.png",
+    "/favicon/android-icon-512x512.png",
+    "/favicon/manifest.json"
 ];
 
 self.addEventListener("install", event => {
+    self.skipWaiting();
+
     event.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
             return cache.addAll(urlsToCache);
@@ -20,16 +25,19 @@ self.addEventListener("activate", event => {
         caches.keys().then(keys =>
             Promise.all(
                 keys.map(key => {
-                    if (key !== CACHE_NAME) {
+                    if (key !== CACHE_NAME)
                         return caches.delete(key);
-                    }
                 })
             )
         )
     );
+
+    self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
+    if (event.request.method !== "GET") return;
+
     event.respondWith(
         caches.match(event.request).then(response => {
             return response || fetch(event.request);
